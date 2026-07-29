cd871c335fe1710638af8af229356846f9152727
Yashwant Patil <106667432+yashwant-patil-454@users.noreply.github.com>
2026-07-29 16:41:43 +0530

    Time: 3 ms (79.99%), Space: 45.5 MB (50.21%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,14 +1,14 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int n = triangle.size();
-        int[] dp = new int[n + 1];
+        int[] arr = new int[n + 1];
 
         for (int i = n - 1; i >= 0; i--) {
             for (int j = 0; j <= i; j++) {
-                dp[j] = Math.min(dp[j], dp[j + 1]) + triangle.get(i).get(j);
+                arr[j] = Math.min(arr[j], arr[j + 1]) + triangle.get(i).get(j);
             }
         }
 
-        return dp[0];
+        return arr[0];
     }
 }
