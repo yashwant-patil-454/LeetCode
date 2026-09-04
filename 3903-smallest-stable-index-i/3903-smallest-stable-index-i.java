f3f16adefe1c949ddeaffb9515051b2276e9514c
Yashwant Patil <106667432+yashwant-patil-454@users.noreply.github.com>
2026-09-04 08:07:06 +0530

    Time: 1 ms (99.79%), Space: 46.1 MB (88.3%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
@@ -1,28 +1,33 @@
 class Solution {
     public int firstStableIndex(int[] nums, int k) {
-        for (int i = 0; i < nums.length; i++) {
-            int max = Integer.MIN_VALUE;
-            int min = Integer.MAX_VALUE;
+        int n = nums.length;
 
-            int j = i;
-            while (j >= 0) {
-                if (nums[j] >= max) {
-                    max = nums[j];
-                }
-                j--;
-            }
-            int l = i;
-            while (l <= nums.length - 1) {
-                if (nums[l] <= min) {
-                    min = nums[l];
-                }
-                l++;
-            }
-            int diff = max - min;
-            if (diff <= k) {
+        int[] prefixMax = new int[n];
+        int[] suffixMin = new int[n];
+
+        // Build prefix maximum
+        prefixMax[0] = nums[0];
+
+        for (int i = 1; i < n; i++) {
+            prefixMax[i] = Math.max(prefixMax[i - 1], nums[i]);
+        }
+
+        // Build suffix minimum
+        suffixMin[n - 1] = nums[n - 1];
+
+        for (int i = n - 2; i >= 0; i--) {
+            suffixMin[i] = Math.min(suffixMin[i + 1], nums[i]);
+        }
+
+        // Find the smallest stable index
+        for (int i = 0; i < n; i++) {
+            int instability = prefixMax[i] - suffixMin[i];
+
+            if (instability <= k) {
                 return i;
             }
         }
+
         return -1;
     }
 }
