a47f6c1b4a85b2d35a05e884acbfb8731f35a5df
Yashwant Patil <106667432+yashwant-patil-454@users.noreply.github.com>
2026-09-13 15:58:23 +0530

    Time: 0 ms (100%), Space: 47.6 MB (42.14%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -0,0 +1,73 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+
+        boolean isFirstRowZero = false;
+        boolean isFirstColumnZero = false;
+
+        int row = matrix.length;
+        int col = matrix[0].length;
+
+        for (int i = 0; i < row; i++) {
+            if (matrix[i][0] == 0) {
+                isFirstRowZero = true;
+            }
+        }
+
+        for (int i = 0; i < col; i++) {
+            if (matrix[0][i] == 0) {
+                isFirstColumnZero = true;
+            }
+        }
+
+        for (int i = 1; i < row; i++) {
+            for (int j = 1; j < col; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+
+        }
+
+        // for (int i = 1; i < row; i++) {
+        //     for (int j = 1; j < col; j++) {
+        //         if (matrix[i][j] == 0) {
+        //             matrix[i][0] = 0;
+        //             matrix[0][j] = 0;
+        //         }
+        //     }
+
+        // }
+
+        for (int i = 1; i < row; i++) {
+            if (matrix[i][0] == 0) {
+                for (int j = 1; j < col; j++) {
+                    matrix[i][j] = 0;
+                }
+            }
+
+        }
+
+        for (int j = 1; j < col; j++) {
+            if (matrix[0][j] == 0) {
+                for (int i = 1; i < row; i++) {
+                    matrix[i][j] = 0;
+                }
+            }
+
+        }
+
+        if (isFirstRowZero) {
+            for (int i = 0; i < row; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+
+        if (isFirstColumnZero) {
+            for (int i = 0; i < col; i++) {
+                matrix[0][i] = 0;
+            }
+        }
+        //  return matrix;
+    }
+}
